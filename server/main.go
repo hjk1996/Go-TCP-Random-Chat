fd1d2b9189214711f6d04a69b5839261adf916f5
hjk1996 <dunhill741@naver.com>
2024-06-16 23:55:07 +0900

    add server config

--- server/main.go
+++ server/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-
 	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-
-	fmt.Println("Initializing the chat server...")
-	listener, err := net.Listen("tcp", "localhost:8888")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-	defer listener.Close()
 
 	redisClient := redis.NewClient(
 		&redis.Options{
@@ -25,22 +14,15 @@ func main() {
 		},
 	)
 
-	hostId := uuid.New().String()
-	server := NewServer(hostId, redisClient)
+	server := NewServer(
+		ServerConfig{
+			Address:     "localhost",
+			Port:        8888,
+			HostID:      uuid.New().String(),
+			RedisClient: redisClient,
+		},
+	)
 
 	server.Run()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to read connection from %s", conn.RemoteAddr().String())
-			continue
-		}
-		log.Printf("New client from %s has join the server", conn.RemoteAddr().String())
-		client := NewClient(conn, server.ClientHandler.ComChan)
-		server.AddClient(client)
-		go client.ReadInput()
-
-	}
 
 }
--- server/server.go
+++ server/server.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
 	"sync"
+
 	"github.com/redis/go-redis/v9"
 )
 
 type Server struct {
+	Address             string
+	Port                int
 	HostId              string
 	Clients             map[string]*Client
 	ClientHandler       *ClientHandler
@@ -16,17 +22,26 @@ type Server struct {
 	ctx                 context.Context
 }
 
-func NewServer(HostId string, redisClient *redis.Client) *Server {
+type ServerConfig struct {
+	Address     string
+	Port        int
+	HostID      string
+	RedisClient *redis.Client
+}
+
+func NewServer(conf ServerConfig) *Server {
 	ctx := context.Background()
 
 	server := &Server{
-		HostId:  HostId,
+		Address: conf.Address,
+		Port:    conf.Port,
+		HostId:  conf.HostID,
 		Clients: make(map[string]*Client),
 		ClientHandler: &ClientHandler{
 			ComChan: make(chan Command),
 		},
 		RedisMessageHandler: &RedisMessageHandler{},
-		RedisClient:         redisClient,
+		RedisClient:         conf.RedisClient,
 		mutex:               sync.RWMutex{},
 		ctx:                 ctx,
 	}
@@ -42,6 +57,28 @@ func (server *Server) AddClient(client *Client) {
 }
 
 func (s *Server) Run() {
+	fmt.Println("Initializing the chat server...")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Address, s.Port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	defer listener.Close()
+
 	go s.ClientHandler.HandleClientMessage()
 	go s.RedisMessageHandler.HandleRedisMessage()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to read connection from %s", conn.RemoteAddr().String())
+			continue
+		}
+		log.Printf("New client from %s has join the server", conn.RemoteAddr().String())
+		client := NewClient(conn, s.ClientHandler.ComChan)
+		s.AddClient(client)
+		go client.ReadInput()
+
+	}
+
 }
